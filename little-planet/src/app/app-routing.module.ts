55cb7dcff5a2289948cf7839e2a7fb67122d89b7
Yiwen Wang <lexieyww@gmail.com>
2021-04-21 02:29:24 +0800

    Navigation Working

--- little-planet/src/app/app-routing.module.ts
+++ little-planet/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { GoalSettingComponent } from './components/goal-setting/goal-setting.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { RewardsComponent } from './components/rewards/rewards.component';
+import { TrackerComponent } from './components/tracker/tracker.component';
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'goal-setting',component:GoalSettingComponent},
+  {path:'registration',component:RegistrationComponent},
+  {path:'rewards',component:RewardsComponent},
+  {path:'tracker',component:TrackerComponent},
+  {path:'**',redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
