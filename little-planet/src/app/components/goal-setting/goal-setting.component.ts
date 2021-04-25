619512eb0de59d2ab629190a637ef5f7fa31febb
Xiaozhou Song <sxz1719@163.com>
2021-04-25 17:39:47 +0100

    4.25

--- little-planet/src/app/components/goal-setting/goal-setting.component.ts
+++ little-planet/src/app/components/goal-setting/goal-setting.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-goal-setting',
@@ -9,18 +11,31 @@ export class GoalSettingComponent implements OnInit {
 
   // public startDay: any = new Date();
 
+  public goal:any={
+      goal_name:null,
+      clicks_num_needed:null,
+      description:null
+    };
 
-  constructor() { 
+
+  constructor(public router:Router) {
     // console.log(this.startDay);
   }
 
   ngOnInit(): void {
+    this.goal.goal_name='12345';
+  }
+
+  refresh(){
+    this.goal.goal_name=null;
+    this.goal.clicks_num_needed=null;
+    this.goal.description=null;
   }
 
   // doSubmit() {
   //   // let goalnameDom: any = document.getElementById("goal_name");
-  //   // console.log(goalnameDom.value); 
+  //   // console.log(goalnameDom.value);
 
-  // } 
+  // }
 
 }
--- little-planet/src/app/components/tracker/tracker.component.ts
+++ little-planet/src/app/components/tracker/tracker.component.ts
@@ -9,6 +9,21 @@ export class TrackerComponent implements OnInit {
 
   public title = "this is a progress tracker";
 
+  public goal_list:any[]=[
+    {
+      goal_num:0,
+      goal_name:'name 0',
+    },
+    {
+      goal_num:1,
+      goal_name:'name 1',
+    },
+    {
+      goal_num:2,
+      goal_name:'name 2',
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
