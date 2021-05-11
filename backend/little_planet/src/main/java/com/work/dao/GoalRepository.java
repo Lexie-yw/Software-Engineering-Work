0bf85a2aecc06c8cff760603a973b42c3cae4b07
jcwvvv <73413798+jcwvvv@users.noreply.github.com>
2021-05-11 21:59:14 +0800

    Delete backend/little_planet directory

--- backend/little_planet/src/main/java/com/work/dao/GoalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.work.dao;
-
-import com.work.entity.Goal;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GoalRepository extends MongoRepository<Goal,String> {
-}
--- backend/little_planet/src/main/java/com/work/service/RewardService.java
+++
@@ -1,9 +0,0 @@
-package com.work.service;
-
-import com.work.entity.Reward;
-
-
-
-public interface RewardService {
-   Reward getReward();
-}
